ba3a74a99c491dc4fc243cfbef61d4963a2d31d4
ItzArcticBlade <150342701+itzmyatic@users.noreply.github.com>
2024-08-20 13:35:48 +0800

    whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -2,15 +2,15 @@ import pywhatkit
 
 # Variables
 phone_numer = ''
-group_id = ''
-message = 'Write the message here'
-time_hour = 14
+group_id = 'LXs4r8zHxU1Bw40zkekL0N'
+message = 'zclick'
+time_hour = 5
 time_minute = 50
-waiting_time_to_send = 15
+waiting_time_to_send = 1
 close_tab = True
 waiting_time_to_close = 2
 
-mode = "contact"
+mode = "group"
 
 if mode == "contact":
     # Send a WhastApp message to an specific contact
